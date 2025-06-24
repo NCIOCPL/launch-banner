d5b8e908527818f53fdbbba52674a050cabdefac
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2025-06-24 13:29:48 -0400

    (#11) Bump Dependencies
    - Forced upgrade to vite 6 - things broke
    - Vite 6 requires Node >=19, so I bumped to Node 22.
    - Vite 6 changed the scss API, so you must use loadPaths
      instead of includePaths.
    
    Closes #11

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer'
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
 	base: process.env.PUBLIC_PATH
 		? process.env.PUBLIC_PATH
 		: '/',
 	build: {
-		minify: true,
-		cssMinify: true,
+		minify: true
 	},
 	css: {
 		postcss: {
@@ -19,7 +22,7 @@ export default defineConfig({
 		preprocessorOptions: {
 			scss: {
 				quietDeps: true,
-				includePaths: [
+				loadPaths: [
 					path.join(
 						__dirname,
 						'node_modules/@nciocpl/ncids-css/packages'
